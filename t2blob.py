ab271ae4cf352cac57c90a0f5c31c5473896419b
Blake Hannaford <blake.hannaford@gmail.com>
2022-05-18 17:02:31 -0700

    new contour class for book blobs

--- t2blob.py
+++ t2blob.py
@@ -2,11 +2,34 @@ import cv2
 import numpy as np
 from PIL import Image
 import sys
+import glob as gb
 
 import book_parms as bpar
 import book_classes as bc
 
+class bblob:  # book blobs
+    def __init__(self,c):
+        self.contour = c
+        self.label = []
+        self.M = cv2.moments(self.contour)
+        if self.M['m00'] > 0:
+            cx = int(self.M['m10']/self.M['m00'])
+            cy = int(self.M['m01']/self.M['m00'])
+        else:
+            cx = 20
+            cy = 20 
+        self.centerpoint = (cx,cy)     
+        self.perim = cv2.arcLength(self.contour, True)
+        self.area  = cv2.contourArea(self.contour)
+        self.rect = cv2.minAreaRect(self.contour)
+        self.box = np.int0(cv2.boxPoints(self.rect))
+        self.elong = box_elong(self.box)
+        self.boxiness = boxy(self.contour,self.box)
+
 def getBookBlobs(imC):
+    if len(imC.ishape()) != 2:
+        print('getBookBlobs: input should be gray not BGR')
+        quit()
     im = imC.image
     #blank = imC.blank()
     blank = cv2.imread('tcolor.png')
@@ -18,105 +41,34 @@ def getBookBlobs(imC):
     im2 = im.copy()
     
     origcontours = []
-    bookcontours = []
     boxycontours = []
     othercontours = []
     
-
     ndc = 0
     nlc = 0
     for i in range(len(contours)):
         #print('\nLooking at Contour ',i)
         nlc +=1
-        c = contours[i]
-        moms = cv2.moments(c)
-        perim = cv2.arcLength(c, True)
-        area  = cv2.contourArea(c)
-        #elong = perim / np.sqrt(area)
-        rect = cv2.minAreaRect(c)
-        box = cv2.boxPoints(rect)
-        box = np.int0(box)
-        elong = box_elong(box)
-        boxiness = boxy(c,box)
-        #hull  = cv2.convexHull(c)
-        #if elong > 7 and elong < 12 and  area > 5000:
-        if area > bpar.area_min  and elong > bpar.elong_min  and elong < bpar.elong_max:
-            if area > 2.0*(bpar.area_min) :
+        c = bblob(contours[i]) 
+        if c.area > bpar.area_min  and c.elong > bpar.elong_min  and c.elong < bpar.elong_max:
+            if c.area > 2.0*(bpar.area_min) :
                 col = bpar.colors['red']
             else:
                 col = bpar.colors['green']
               
             origcontours.append(c)
-            #cv2.drawContours(blank, [box], -1, col, 3)
-            bookcontours.append([box])
             ndc += 1
-        elif area > bpar.area_min/2 and boxiness >= bpar.enough_corners:
-            boxycontours.append([c])
-            bookcontours.append([box])
+        elif c.area > bpar.area_min/2 and c.boxiness >= bpar.enough_corners:
+            boxycontours.append([c]) 
             ndc += 1
             
-        else:
-            if area > bpar.noise_area_threshold:
-                othercontours.append(c)
-
-        #
-        #   find "boxy" blobs that are smaller than area_min
-        #
-        
-        ##print('Moments:     ', moms)
-        #print('Area:        {:8.1f}'.format(  area))
-        #print('Perimeter    {:8.1f}'.format( perim))
-        #print('Elongation   {:8.3f}'.format( elong))
-        
-    #if ndc > 0:
-        #title = 'contours, hulls'
-        #cv2.imshow(title,blank)
-        #cv2.waitKey(-1)
-    #else:
-        #print('\n\n              No Contours Match \n\n')
+        elif c.area > bpar.noise_area_threshold:
+            othercontours.append(c)
+ 
     print('\n\n   Looked at {:} unfiltered contours.\n\n'.format(nlc))
-    return bookcontours, boxycontours, origcontours, othercontours
+    return  origcontours, boxycontours, othercontours
     
     
-###
-##
-#  2D feature calls
-#   findContours(binaryImage, contours, RETR_LIST, CHAIN_APPROX_NONE);
-#   moments()    Moments moms = moments(contours[contourIdx]);
-#   perimeter = arcLength(contours[contourIdx], true);
-#   convexHull(contours[contourIdx], hull);
-#    double area = contourArea(contours[contourIdx]);
-    #double hullArea = contourArea(hull);
-#
-
-####
-
-if __name__== '__main__':
-    #if len(sys.argv) != 2:
-        #print ('supply an arg')
-        #quit()
-        
-    bookcs = []
-    for i in range(20):
-        fname = 'blobSet{:02d}.png'.format(int(i))
-        print('Opening: ', fname)
-        im1 = cv2.imread(fname) 
-        im1Image = bc.bookImage(im1,bpar.scale)
-        
-        bcont =  getblobs(im1Image)
-        for b in bcont:
-            bookcs.append(b)
-            
-    print ('\n\n  Found {:} book contours \n\n'.format(len(bookcs)))
-    
-    col = bpar.colors['red']
-    blank = cv2.imread('tcolor.png')
-    for b in bookcs:
-        cv2.drawContours(blank, b, -1, col, 3)
-
-    cv2.imshow('title',blank)
-    cv2.waitKey(-1)
-
 ##############
 #
 #  does the contour resemble part of a rectangle??
@@ -142,46 +94,6 @@ def boxy(blob, box):
 
             
 
-def boxy0(blob, box):
-    rmin_box = 999999999
-    rmax_box = 0
-    cmin_box = rmin_box
-    cmax_box = 0
-    rmin_blob = 999999999
-    rmax_blob = 0
-    cmin_blob = rmin_blob
-    cmax_blob = 0
-    
-    for p in box:
-        if p[0] < rmin_box:
-            rmin_box = p[0]
-        if p[0] > rmax_box:
-            rmax_box = p[0]
-        if p[1] < cmin_box:
-            cmin_box = p[1]
-        if p[1] > cmax_box:
-            cmax_box = p[1]
-        
-    for p in blob:
-        if p[0] < rmin_blob:
-            rmin_blob = p[0]
-        if p[0] > rmax_blob:
-            rmax_blob = p[0]
-        if p[1] < cmin_blob:
-            cmin_blob = p[1]
-        if p[1] > cmax_blob:
-            cmax_blob = p[1]
-        
-    box_corners = [[rmin_box, cmin_box],[rmin_box, cmax_box],[rmax_box, cmin_box],[rmax_box, cmax_box]]
-    blob_corners = [[rmin_blob, cmin_blob],[rmin_blob, cmax_blob],[rmax_blob, cmin_blob],[rmax_blob, cmax_blob]]
-    
-    score = 0
-    for xc in box_corners:
-        for lc in blob_corners:
-            if pdist(xc,lc) < 10:
-                score += 1
-    return score
-
 def box_elong(b):
     l1 = pdist(b[0],b[1])
     l2 = pdist(b[0],b[3])
@@ -199,3 +111,52 @@ def pdist(p1,p2):
     r = np.sqrt(d1*d1+d2*d2)
     return r
 
+
+###
+##
+#  2D feature calls
+#   findContours(binaryImage, contours, RETR_LIST, CHAIN_APPROX_NONE);
+#   moments()    Moments moms = moments(contours[contourIdx]);
+#   perimeter = arcLength(contours[contourIdx], true);
+#   convexHull(contours[contourIdx], hull);
+#    double area = contourArea(contours[contourIdx]);
+    #double hullArea = contourArea(hull);
+#
+
+####
+
+if __name__== '__main__':
+    if len(sys.argv) != 2:
+        print ('supply an arg')
+        quit()
+    
+    imagefilename = 'blobSet*_{:}.png'.format(sys.argv[1])
+    print('t2blob: looking at : ',imagefilename, ' in ', bpar.tmp_img_path)
+        
+    filenames = gb.glob(bpar.tmp_img_path+imagefilename)
+    if len(filenames) < 1:
+        print('There are no files')
+        quit() 
+        
+    bookcs = []
+    for fname in filenames:
+        #fname = 'blobSet{:02d}.png'.format(int(i))
+        print('Opening: ', fname)
+        im1 = cv2.imread(fname) 
+        im1Image = bc.bookImage(im1,bpar.scale)
+        im1Image.image = im1Image.gray()
+        origcontours, boxycontours, othercontours =  getBookBlobs(im1Image) 
+        bookcs = bookcs + origcontours
+        
+        
+    print ('\n\n  Found {:} book contours \n\n'.format(len(bookcs)))
+    
+    c_red = bpar.colors['red']
+    c_blu = bpar.colors['blue']
+    blank = cv2.imread('tcolor.png')
+    for b in bookcs:
+        cv2.drawContours(blank, b.contour, -1, c_red, 3)
+        cv2.drawContours(blank, [ b.box ], -1, c_blu, 3)
+
+    cv2.imshow('title',blank)
+    cv2.waitKey(-1)
